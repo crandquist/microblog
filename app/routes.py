5cb48c2261977c28f88dd09203310c44d428de79
Madi <crandquist@gmail.com>
2020-06-27 14:57:28 -0700

    Updated index.html and routes.py to reflect current user functionality.

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Madi'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,7 +21,7 @@ def index():
             'body': 'Beautiful day in Portland!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
