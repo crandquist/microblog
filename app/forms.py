267284231530488689fad60e031e24ff7c44d2e2
Madi <crandquist@gmail.com>
2020-06-27 20:28:05 -0700

    Added edit profile form and link to edit profile on user profile template.

--- app/forms.py
+++ app/forms.py
@@ -28,5 +28,5 @@ class RegistrationForm(FlaskForm):
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=([DataRequired()]))
-    about_me = TextAreaField('About Me', validators=(Length(min=0, max=140)))
+    about_me = TextAreaField('About Me', validators=([Length(min=0, max=140)]))
     submit = SubmitField('Submit')
