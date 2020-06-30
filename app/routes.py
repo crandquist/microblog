0172ec179672df99e2503ae64e960bebd7e99798
Madi <crandquist@gmail.com>
2020-06-29 20:43:24 -0700

    Added method arguments to the '/' and '/index' routes so that it accepts GET and POST.

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,8 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, P
 from app.models import User, Post
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
