f67ce6c67d77d12eb5de80269ead99137cda20b9
Madi <crandquist@gmail.com>
2020-07-02 15:49:12 -0700

    Added langdetect and removed guess_language which was not working. Also updated and compiled the translation files.

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
-from guess_language import guess_language
+from langdetect import detect
 from app import app, db
 from app.email import send_password_reset_email
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
@@ -25,7 +25,7 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        language = guess_language(form.post.data)
+        language = detect(form.post.data)
         if language == 'UNKNOWN' or len(language) > 5:
             language = ''
         post = Post(body=form.post.data, author=current_user, language=language)
