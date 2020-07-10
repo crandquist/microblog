1eaa585056e1f2dd35125cfb534c181cfb784c9d
Madi <crandquist@gmail.com>
2020-07-10 13:21:59 -0700

    Fixed error in get_rq_job function.

--- app/models.py
+++ app/models.py
@@ -205,7 +205,7 @@ class Task(db.Model):
 
     def get_rq_job(self):
         try:
-            get_rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
         except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
             return None
         return rq_job
