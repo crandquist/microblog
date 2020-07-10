474aef0c607562fa4acb9c263afa1d4065e45dbd
Madi <crandquist@gmail.com>
2020-07-10 15:55:39 -0700

    Finished writing the export posts function.

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,10 @@
+import json
 import time
+from flask import render_template
 from rq import get_current_job
 from app import create_app, db
-from app.models import Task
+from app.email import send_email
+from app.models import User, Post, Task
 
 
 app = create_app()
@@ -20,8 +23,27 @@ def _set_task_progress(progress):
 
 def export_posts(user_id):
     try:
-        # read user posts from database
-        # send email with data to user
+        user = User.query.get(user_id)
+        _set_task_progress(0)
+        data = []
+        i = 0
+        total_posts = user.posts.count()
+        for post in user.posts.order_by(Post.timestamp.asc()):
+            data.append({'body': post.body,
+                        'timestamp': post.timestamp.isoformat() + 'Z'})
+            time.sleep(5)
+            i += 1
+            _set_task_progress(100 * i // total_posts)
+
+        send_email('[Microblog] Your blog posts',
+                    sender=app.config['ADMINS'][0], 
+                    recipients=[user.email], 
+                    text_body=render_template('email/export_posts.txt', user=user), 
+                    html_body=render_template('email/export_posts.html', user=user),
+                    attachments=[('posts.json',
+                                'application/json',
+                                json.dumps({'posts': data}, indent=4))],
+                    sync=True)
     except:
         app.logger.error('Unhandled exception', exc_info=sys.exc_info())
     finally:
