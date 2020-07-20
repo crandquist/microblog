dcbf6b682112fc3d9068c0e41c931a5940751082
Madi <crandquist@gmail.com>
2020-07-20 13:55:50 -0700

    Added get_users function to the api and fixed some typing errors in models.py.

--- app/api/users.py
+++ app/api/users.py
@@ -1,14 +1,19 @@
+from flask import jsonify, request
 from app.api import bp
+from app.models import User
 
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    pass
+    return jsonify(User.query.get_or_404(id).to_dict())
 
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
 
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
@@ -16,7 +21,7 @@ def get_followers(id):
     pass
 
 
-@bp.route('/users/<int:id/followed', methods=['GET'])
+@bp.route('/users/<int:id>/followed', methods=['GET'])
 def get_followed(id):
     pass
 
